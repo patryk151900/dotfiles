426ab8143ac56cfa28917b4c5cbe2ae0e0b6eff5
patryk151900 <patryk151900@gmail.com>
2016-10-25 19:32:42 +0200

    safe dotfiles installation

--- install.sh
+++ install.sh
@@ -4,16 +4,54 @@
 SCRIPT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 SCRIPT_PATH=$(dirname $SCRIPT)
 
+# store timestamp based sufix for backed up files
+TIMESTAMP="$(date +%s)"
+
+# $1 - file to back up
+function backup_orig_file {
+	if [ -e $1 ]
+		then
+			echo "target file ($1) already exist, backing up..."
+			TARGET=$1_orig_$TIMESTAMP 
+			echo "moving $1 to $TARGET"
+			mv $1 $TARGET 
+	fi
+}
+
+# $1 - target directory
+# $2 - target file name
+# $3 - source file name (relative to script path == main of dotfiles repo)
+function install_file {
+	echo "Installing $2..."
+	if [ ! -L $1/$2 ]
+		then echo "symbolic link does not exist yet, creating"
+		backup_orig_file $1/$2
+		ln -s $SCRIPT_PATH/$3 $1/$2
+	else
+		echo "symbolic link already exist and will not be changed"
+		ls -al $1/$2
+	fi
+}
+
+echo "Starting installation of dot files..."
+
 # vim
-ln -s $SCRIPT_PATH/.vim .vim
-ln -s $SCRIPT_PATH/.vimrc .vimrc
+install_file ~ .vim .vim
+install_file ~ .vimrc .vimrc
 
 # tmux
-ln -s $SCRIPT_PATH/.tmux.conf .tmux.conf
+install_file ~ .tmux.conf .tmux.conf
 
 # bashrc
-ln -s $SCRIPT_PATH/.bashrc_virtual_ubuntu_work .bashrc
+install_file ~ .bashrc .bashrc_virtual_ubuntu_work
 
-# clone vundle if not yet exist
-git clone https://github.com/VundleVim/Vundle.vim .vim/bundle/vundle
+# clone vundle
+VUNDLE=~/.vim/bundle/vundle
+echo "Clonning vundle repo into $vundle..."
+if [ -e $VUNDLE ]
+	then echo "vundle already cloned (at least directory exists)"
+else
+	git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/vundle
+fi
 
+echo "installation completed..."
