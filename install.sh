d593ee7f39cde1cc8ecac1775c22007fcbef7556
Patryk P <patryk151900@gmail.com>
2020-10-18 12:16:59 +0200

    reorganized install.sh file and installation of link to knowledge repo

--- install.sh
+++ install.sh
@@ -17,28 +17,36 @@ TIMESTAMP="$(date +%s)"
 #     $1 - file to back up
 function backup_orig_file {
 	if [ -e $1 ]
-		then
-			echo "target file ($1) already exist, backing up..."
-			TARGET=$1_orig_$TIMESTAMP 
-			echo "moving $1 to $TARGET"
-			mv $1 $TARGET 
+	then
+		echo "...target file ($1) already exist, backing up..."
+		TARGET=$1_orig_$TIMESTAMP 
+		echo "...moving $1 to $TARGET"
+		mv $1 $TARGET 
 	fi
 }
-
 
 # brief: installs the file as symbolic link; backs up original file
 # parameters:
 #    $1 - target directory
 #    $2 - target file name
-#    $3 - source file name (relative to script path == main of dotfiles repo)
+#    $3 - source directory
+#    $4 - source file name (relative to script path == main of dotfiles repo)
 function install_file {
 	echo "Installing $2..."
 	if [ ! -L $1/$2 ]
-		then echo "symbolic link does not exist yet, creating"
-		backup_orig_file $1/$2
-		ln -s $SCRIPT_PATH/$3 $1/$2
+	then 
+		echo "...symbolic link does not exist yet, creating..."
+		if [ ! -e $3/$4 ]
+		then
+			echo "...ERROR: source file or directory does not exist!!!"
+		else		
+			backup_orig_file $1/$2
+			ln -s $3/$4 $1/$2
+			echo "...created:"
+			ls -al $1/$2
+		fi
 	else
-		echo "symbolic link already exist and will not be changed"
+		echo "...symbolic link already exists and will not be changed:"
 		ls -al $1/$2
 	fi
 }
@@ -47,25 +55,31 @@ function install_file {
 echo "Starting installation of dot files..."
 
 # vim
-install_file ~ .vim .vim
-install_file ~ .vimrc .vimrc
+install_file ~ .vim $SCRIPT_PATH .vim
+install_file ~ .vimrc $SCRIPT_PATH .vimrc
 
 # tmux
-install_file ~ .tmux.conf .tmux.conf
+install_file ~ .tmux.conf $SCRIPT_PATH .tmux.conf
 
 # bashrc
-install_file ~ .bashrc .bashrc_virtual_ubuntu_work
+install_file ~ .bashrc $SCRIPT_PATH .bashrc_ubuntu_raspbian
 
 # install bash aliasses
-install_file ~ .bash_aliases .bash_aliases
+install_file ~ .bash_aliases $SCRIPT_PATH .bash_aliases
 
-# clone vundle
+# install .knowledge repository
+install_file ~ .knowledge ~ repos/knowledge
+
+
+# clone vundle for vim purpose
 VUNDLE=~/.vim/bundle/vundle
 echo "Clonning vundle repo into $vundle..."
 if [ -e $VUNDLE ]
-	then echo "vundle already cloned (at least directory exists)"
+	then echo "...vundle already cloned (at least directory exists)"
 else
 	git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/vundle
+	echo "...cloned"
+	echo "...open vim and type :PluginInstall"
 fi
 
 echo "Installation completed!"
